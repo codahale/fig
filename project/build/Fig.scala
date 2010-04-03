eb9cbc89632cf1185cf5a44739d7bfc2549a5cfe
Coda Hale <coda.hale@gmail.com>
2010-04-03 15:38:14 -0700

    Small SBT cleanup.

--- project/build/Fig.scala
+++ project/build/Fig.scala
@@ -1,34 +1,28 @@
 class Fig(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with posterous.Publish {
   /**
-   * Home Repo
+   * Publish the source as well as the class files.
    */
-  override def managedStyle = sbt.ManagedStyle.Maven
-  val publishTo = sbt.Resolver.file("Local Cache", ("." / "target" / "repo").asFile)
+  override def packageSrcJar= defaultJarPath("-sources.jar")
+  val sourceArtifact = sbt.Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
 
   /**
    * Publish to a local temp repo, then rsync the files over to repo.codahale.com.
    */
+  override def managedStyle = sbt.ManagedStyle.Maven
+  val publishTo = sbt.Resolver.file("Local Cache", ("." / "target" / "repo").asFile)
   def publishToLocalRepoAction = super.publishAction
   override def publishAction = task {
     log.info("Uploading to repo.codahale.com")
     sbt.Process("rsync", "-avz" :: "target/repo/" :: "codahale.com:/home/codahale/repo.codahale.com" :: Nil) ! log
     None
   } describedAs("Publish binary and source JARs to repo.codahale.com") dependsOn(test, publishToLocalRepoAction)
-
-  /**
-   * Publish the source as well as the class files.
-   */
-  override def packageSrcJar= defaultJarPath("-sources.jar")
-  val sourceArtifact = sbt.Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
   
   /**
    * Dependencies
    */
   val scalaToolsSnapshots = "scala-tools.org Snapshots" at "http://scala-tools.org/repo-snapshots"
-
   val liftJson = "net.liftweb" % "lift-json" % "2.0-scala280-SNAPSHOT" withSources() intransitive()
   val paranamer = "com.thoughtworks.paranamer" % "paranamer" % "2.0" withSources() intransitive()
-
   val scalaTest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
 }
