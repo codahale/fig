d8bab8af5790cb437d89b4338d549e60ae097ff4
Coda Hale <coda.hale@gmail.com>
2010-07-15 18:41:37 -0700

    Version bump to 2.8.0 final and 1.0.2.

--- project/build/Fig.scala
+++ project/build/Fig.scala
@@ -15,7 +15,7 @@ class Fig(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with posterous
    * Dependencies
    */
   val scalaToolsSnapshots = "scala-tools.org Snapshots" at "http://scala-tools.org/repo-snapshots"
-  val liftJson = "net.liftweb" % "lift-json" % "2.0-scala280-SNAPSHOT" withSources() intransitive()
+  val liftJson = "net.liftweb" %% "lift-json" % "2.1-SNAPSHOT" withSources() intransitive()
   val paranamer = "com.thoughtworks.paranamer" % "paranamer" % "2.0" withSources() intransitive()
-  val scalaTest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test" withSources() intransitive()
+  val scalaTest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test" withSources() intransitive()
 }
