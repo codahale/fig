632126d3125ff1e5300ca1a74a94d83942db09c6
Coda Hale <coda.hale@gmail.com>
2011-05-22 22:10:22 -0700

    Upgrade to Jerkson 0.2.2.

--- project/build/FigProject.scala
+++ project/build/FigProject.scala
@@ -28,7 +28,7 @@ class FigProject(info: ProjectInfo) extends DefaultProject(info)
   /**
    * Dependencies
    */
-  val jerkson = "com.codahale" %% "jerkson" % "0.2.1"
+  val jerkson = "com.codahale" %% "jerkson" % "0.2.2"
 
   /**
    * Test Dependencies
