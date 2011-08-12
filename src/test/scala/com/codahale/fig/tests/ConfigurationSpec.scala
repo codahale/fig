0e6858f1a54f245e7d6828cc20931337e4ba0b0a
Coda Hale <coda.hale@gmail.com>
2011-08-11 17:35:52 -0700

    Upgrade to simplespec 0.4.0.

--- src/test/scala/com/codahale/fig/tests/ConfigurationSpec.scala
+++ src/test/scala/com/codahale/fig/tests/ConfigurationSpec.scala
@@ -1,43 +1,44 @@
 package com.codahale.fig.tests
 
 import com.codahale.simplespec.Spec
+import com.codahale.simplespec.annotation.test
 import com.codahale.fig.{ConfigurationException, Configuration}
 
 class ConfigurationSpec extends Spec {
   class `A configuration file` {
-    private val config = new Configuration("src/test/resources/example.json")
+    val config = new Configuration("src/test/resources/example.json")
 
-    def `should have specific values` = {
+    @test def `has specific values` = {
       config("parent.child.count").as[Int] must beEqualTo(100)
     }
 
-    def `should have optional values` = {
+    @test def `has optional values` = {
       config("parent.child.count").asOption[Int] must beSome(100)
       config("parent.child.woof").asOption[Int] must beNone
     }
 
-    def `should optionally have default values` = {
+    @test def `optionally has default values` = {
       config("parent.dingo").or("yay") must beEqualTo("yay")
     }
 
-    def `should optionally have required values` = {
+    @test def `optionally has required values` = {
       config("parent.child.count").asRequired[Int] must beEqualTo(100)
     }
 
-    def `should throw an informative exception when a required value is missing` = {
+    @test def `throws an informative exception when a required value is missing` = {
       config("parent.child.age").asRequired[Int] must
               throwA(new ConfigurationException("int property parent.child.age not found"))
     }
 
-    def `should have lists of items` = {
+    @test def `has lists of items` = {
       config("parent.child.names").asList[String] must beEqualTo(List("One", "Two", "Three"))
     }
 
-    def `should have maps of items` = {
+    @test def `has maps of items` = {
       config("parent.child.mapped").asMap[Int] must beEqualTo(Map("1" -> 1, "2" -> 2, "3" -> 3))
     }
 
-    def `should have maps of complicated items` = {
+    @test def `has maps of complicated items` = {
       config("parent.child.doubly-mapped").asMap[List[Int]] must
               beEqualTo(Map("1" -> List(1, 2, 3),
                             "2" -> List(2, 3, 4),
